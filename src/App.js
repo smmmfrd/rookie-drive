b473d48a6d7a46b39a74c881021cf1c480617fa5
Sam Mumford <smmmfrd@gmail.com>
2022-09-13 10:37:04 -0700

    Connect Firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { firestore } from "./firebase";
 
 import Navbar from "./components/Navbar";
 
+async function getLanding(){
+  const landing = await firestore.collection('landing').doc('example').get();
+  const data = landing.data();
+  return data;
+}
+
 export default function App() {
-  console.log('working');
+  getLanding().then(res => console.log(res));
   return (
     <div className="App">
       <Navbar />
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app'; 
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDhVbDd2EZSfS7eu7KNEn2cXx3UPtQ0uag",
+    authDomain: "rookie-drive.firebaseapp.com",
+    projectId: "rookie-drive",
+    storageBucket: "rookie-drive.appspot.com",
+    messagingSenderId: "569267608277",
+    appId: "1:569267608277:web:165b0f13fd578bc3c3e05c"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export { auth, firestore}
