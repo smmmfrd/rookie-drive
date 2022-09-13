59337a719dbcb39be7af0f9730a41db966c09b92
Sam Mumford <smmmfrd@gmail.com>
2022-09-13 10:24:58 -0700

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
+
+import Navbar from "./components/Navbar";
+
 export default function App() {
   console.log('working');
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return(
+        <nav>
+            <h1>Rookie Drive</h1>
+            <div>
+                <button>+ New File</button>
+                <button>Sign In</button>
+            </div>
+        </nav>
+    )
+}
