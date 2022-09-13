5c2fa67f838b64a3280aed64224dcb5afeb46269
Sam Mumford <smmmfrd@gmail.com>
2022-09-13 13:53:16 -0700

    Open Doc

--- src/App.js
+++ src/App.js
@@ -4,31 +4,66 @@ import { firestore } from "./firebase";
 import Navbar from "./components/Navbar";
 
 async function getLandingDocs(){
-  console.log('fetching from firestore');
+  console.log('fetching landing from firestore');
   const landingDoc = await firestore.collection('landing').doc('example').get();
   const data = landingDoc.data();
-  return(Object.keys(data).map(docName => docName));
+  return data;
+}
+
+async function getFieldData(docName){
+  console.log('fetching a field from firestore');
+  const fullData = await getLandingDocs();
+  const field = fullData[docName];
+  return field;
 }
 
 export default function App() {
+  const [currentDoc, setCurrentDoc] = useState({});
   const [docNames, setDocNames] = useState([]);
 
   useEffect(() => {
-    getLandingDocs().then(res => setDocNames(res));
+    getLandingDocs().then(res => setDocNames(Object.keys(res).map(docName => docName)));
   }, []);
+
+  function docSelected(docName){
+    getFieldData(docName).then(res => setCurrentDoc(res));
+  }
+
+  function closeCurrentDoc(){
+    setCurrentDoc({});
+  }
+
+  function currentDocElement(){
+    return (
+      <>
+        <button onClick={closeCurrentDoc}>Close Doc</button>
+        <Note doc={currentDoc}/>
+      </>
+    );
+  }
   
   const docElements = docNames.map(docName => (
-    <div key={docName} className="doc-shortcut">
+    <div key={docName} className="doc-shortcut"
+      onClick={() => docSelected(docName)}>
       <h2>{docName}</h2>
     </div>
   ));
 
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <div className="doc-display">
-        {docElements}
-      </div>
-    </div>
+      {currentDoc.type !== undefined ? currentDocElement()
+      : (<div className="doc-display">{docElements}</div>)}
+    </>
   );
+}
+
+function Note({doc}){
+  const paragraphs = Object.keys(doc).filter(key => key !== "type");
+  const paragraphElements = paragraphs.map(p => (<p>{doc[p]}</p>))
+  return(
+    <div>
+      {paragraphElements}
+    </div>
+  )
 }
