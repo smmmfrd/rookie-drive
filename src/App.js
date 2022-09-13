9def37f99cb57eeb189b3e67c5e4ea614fd21dcc
Sam Mumford <smmmfrd@gmail.com>
2022-09-13 12:01:15 -0700

    Doc Display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import { useState, useEffect } from "react";
 import { firestore } from "./firebase";
 
 import Navbar from "./components/Navbar";
 
-async function getLanding(){
-  const landing = await firestore.collection('landing').doc('example').get();
-  const data = landing.data();
-  return data;
+async function getLandingDocs(){
+  console.log('fetching from firestore');
+  const landingDoc = await firestore.collection('landing').doc('example').get();
+  const data = landingDoc.data();
+  return(Object.keys(data).map(docName => docName));
 }
 
 export default function App() {
-  getLanding().then(res => console.log(res));
+  const [docNames, setDocNames] = useState([]);
+
+  useEffect(() => {
+    getLandingDocs().then(res => setDocNames(res));
+  }, []);
+  
+  const docElements = docNames.map(docName => (
+    <div key={docName} className="doc-shortcut">
+      <h2>{docName}</h2>
+    </div>
+  ));
+
   return (
     <div className="App">
       <Navbar />
+      <div className="doc-display">
+        {docElements}
+      </div>
     </div>
   );
 }
