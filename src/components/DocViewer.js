a39654891ba052779fa059f0938d17a7121b85f5
Sam Mumford <smmmfrd@gmail.com>
2022-09-14 13:55:08 -0700

    Textarea Resize

--- src/components/DocViewer.js
+++ src/components/DocViewer.js
@@ -21,7 +21,11 @@ function Note({doc, editing}){
             return arr;
         }
     }, []);
-    const paragraphElements = paragraphs.map(p => (<p>{p}</p>))
+    const paragraphElements = paragraphs.map((p, index) => (
+        <p key={index}>
+            {p}
+        </p>
+    ));
     return(
         <div className="doc-editor">
             {paragraphElements}
@@ -32,21 +36,42 @@ function Note({doc, editing}){
 
 function NoteEditor({ paragraphs }){
     const [value, setValue] = useState('');
+    const [areaHeight, setAreaHeight] = useState(0);
 
     useEffect(() => {
         const text = paragraphs.reduce((cur, p) => {
             return cur + p;
         }, '');
         setValue(text);
+        setAreaHeight(calcTextAreaHeight(text));
     }, [])
+
+    function calcTextAreaHeight(value) {
+        let lines = value.split('\n');
+        let numberOfLineBreaks = lines.length;
+        let numberOfLongLines = lines.reduce((total, line) => {
+            return Math.floor(line.length / 40) + total;
+        }, 0)
+        
+        // min-height + lines x line-height + padding + border
+        let newHeight = 20 + (numberOfLineBreaks + numberOfLongLines) * 20 + 2;
+        return newHeight;
+    }
 
     function handleChange(event) {
         setValue(event.target.value);
+        setAreaHeight(calcTextAreaHeight(event.target.value));
     }
 
     return(
         <div>
-            <textarea className="doc-editor--input" value={value} onChange={handleChange} />
+            <textarea
+                style={{height:`${areaHeight}px`}}
+                className="doc-editor--input"
+                value={value}
+                onChange={handleChange}
+                cols={40}
+            />
         </div>
     )
 }
