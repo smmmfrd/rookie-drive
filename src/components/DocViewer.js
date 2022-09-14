82eae997c1a5f96c376d9f4f025c14ac393e5778
Sam Mumford <smmmfrd@gmail.com>
2022-09-14 13:21:38 -0700

    Editor Display

--- src/components/DocViewer.js
+++ src/components/DocViewer.js
@@ -1,19 +1,52 @@
+import { useState, useEffect } from "react";
+
 export default function DocViewer({closeCurrentDoc, currentDoc}){
+    const [editing, setEditing] = useState(false);
     return (
         <>
-          <button onClick={closeCurrentDoc}>Close Doc</button>
-          <button>Edit Doc</button>
-          <Note doc={currentDoc}/>
+          <div>
+              <button onClick={closeCurrentDoc}>Close Doc</button>
+              <button onClick={() => setEditing(!editing)}>Edit Doc</button>
+          </div>
+          <Note doc={currentDoc} editing={editing}/>
         </>
       );
 }
 
-function Note({doc}){
-  const paragraphs = Object.keys(doc).filter(key => key !== "type");
-  const paragraphElements = paragraphs.map(p => (<p>{doc[p]}</p>))
-  return(
-    <div>
-      {paragraphElements}
-    </div>
-  )
+function Note({doc, editing}){
+    const paragraphs = Object.keys(doc).reduce((arr, key) => {
+        if(key !== "type"){
+            return [...arr, doc[key]];
+        } else {
+            return arr;
+        }
+    }, []);
+    const paragraphElements = paragraphs.map(p => (<p>{p}</p>))
+    return(
+        <div className="doc-editor">
+            {paragraphElements}
+            {editing && <NoteEditor paragraphs={paragraphs} />}
+        </div>
+    )
+}
+
+function NoteEditor({ paragraphs }){
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        const text = paragraphs.reduce((cur, p) => {
+            return cur + p;
+        }, '');
+        setValue(text);
+    }, [])
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    return(
+        <div>
+            <textarea className="doc-editor--input" value={value} onChange={handleChange} />
+        </div>
+    )
 }
