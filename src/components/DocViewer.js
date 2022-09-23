ccedece8878922664e74bcbc42d3cd441f175c72
Sam Mumford <smmmfrd@gmail.com>
2022-09-23 16:21:01 -0700

    Display Meme

--- src/components/DocViewer.js
+++ src/components/DocViewer.js
@@ -39,6 +39,8 @@ export default function DocViewer({closeCurrentDoc, currentDoc, docEdited, delet
                 return <Todo {...customProps}/>;
             case 'rand': 
                 return <RandomDraw {...customProps}/>;
+            case 'meme': 
+                return <MemeGenerator {...customProps}/>;
             default:
                 return null;
         }
@@ -54,4 +56,25 @@ export default function DocViewer({closeCurrentDoc, currentDoc, docEdited, delet
             { docDisplay() }
         </>
     );
+}
+
+function MemeGenerator({doc, editing, docChange}){
+    const {topText, bottomText, img} = doc;
+
+    return(
+        <div className="doc-editor">
+            <div className="doc-editor--display">
+                <div className="meme">
+                    <img className="meme--img" src={img} alt="A randomly generated meme"/>
+                    <h3 className="meme--text top">{topText}</h3>
+                    <h3 className="meme--text bottom">{bottomText}</h3>
+                </div>
+            </div>
+            {editing && 
+                <div className="doc-editor--input-container">
+                    <h2>Editing</h2>
+                </div>
+            }
+        </div>
+    )
 }
